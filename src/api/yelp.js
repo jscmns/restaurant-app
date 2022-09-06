efd77164f4aafea35f80456fe97ef989b8375008
jscmns <camunasluis@gmail.com>
2022-09-06 17:57:18 -0300

    hide env variables

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from "axios";
+import { YELP_KEY } from "@env";
 
 export default axios.create({
 	baseURL: "https://api.yelp.com/v3/businesses",
 	headers: {
-		Authorization:
-			"Bearer qUkbX7ie2QqfvfWg3hBN8FdV2VVqyOc9eY-x2m7U1iKTJHnnFbwavbFLAtYbK4cAcqj0YJMtCQ9hX4fePTW2S6rTLdJnEnlvOijeeElNARKWWL83eqLcDrLxjGIXY3Yx"
+		Authorization: `Bearer ${YELP_KEY}`
 	}
 });
